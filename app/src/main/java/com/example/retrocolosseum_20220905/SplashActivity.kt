d5dca1d3352b191c4b4be4dd79ccadee5e9e1b7c
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-05 07:18:43 +0900

    자동로그인 기능 임시로 구현

--- app/src/main/java/com/example/retrocolosseum_20220905/SplashActivity.kt
+++ app/src/main/java/com/example/retrocolosseum_20220905/SplashActivity.kt
@@ -1,11 +1,43 @@
 package com.example.retrocolosseum_20220905
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
+
+    var isTokenOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            //이프엘스문 한 줄로 코드해봐 인텐트로 어떻게 묶어갈것인가
+            if(isTokenOk){
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+            else{
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+            finish()
+
+        },2500)
+
+
     }
 }
